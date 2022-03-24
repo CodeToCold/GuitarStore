a39971db2372b3766ed570ae3686931d52c682df
CodeToCold <100582691+CodeToCold@users.noreply.github.com>
2022-03-24 12:06:38 +0300

    Add WebApp, search controller and view list of query elements

--- GuitarRepository/GuitarRepository.cs
+++ GuitarRepository/GuitarRepository.cs
@@ -1,7 +1,19 @@
-﻿namespace Store.Memory
-{
-    public class GuitarRepository
-    {
+﻿using System.Linq;
 
+namespace Store.Memory
+{
+    public class GuitarRepository : IGuitarRepository
+    {
+        private readonly Guitar[] guitars = new[]
+        {
+            new Guitar("Gibson", 1),
+            new Guitar("Ibanez", 2),
+            new Guitar("Auchan", 3)
+        };
+
+        public Guitar[] GetAllByQuery(string query)
+        {
+            return guitars.Where(guitar => guitar.Name.Contains(query)).ToArray();
+        }
     }
 }
---
+++ Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IGuitarRepository guitarRepository;
+
+        public SearchController(IGuitarRepository guitarRepository)
+        {
+            this.guitarRepository = guitarRepository;
+        }
+        
+        public IActionResult Index(string query)
+        {
+            var guitars = guitarRepository.GetAllByQuery(query);
+
+                return View(guitars);
+        }
+    }
+}
